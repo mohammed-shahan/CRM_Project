c68d36648462e9dcdadfb9ef3f5ee5f337cc44b9
akshaylals <akshaylal.s@inapp.com>
2022-08-09 16:12:22 +0530

    course list

---
+++ js/courselist.js
@@ -0,0 +1,125 @@
+var courses = [
+    {
+        code: 'CRM010',
+        name: 'Physics',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum officiis maiores iure odit nisi corporis corrupti, similique ea provident nam dolorum ex voluptatem amet, sint quidem? Cum nulla quo ab.',
+        trainer: 'Abhilash Nelson',
+        duration: 20, // days
+        startDate: '26/05/2022',
+        amount: 80000,
+    },
+    {
+        code: 'CRM011',
+        name: '',
+        description: '',
+        trainer: '',
+        duration: 0, // days
+        startDate: '',
+        amount: 0,
+    },
+    {
+        code: '',
+        name: '',
+        description: '',
+        trainer: '',
+        duration: 0, // days
+        startDate: '',
+        amount: 0,
+    },
+    {
+        code: '',
+        name: '',
+        description: '',
+        trainer: '',
+        duration: 0, // days
+        startDate: '',
+        amount: 0,
+    },
+    {
+        code: '',
+        name: '',
+        description: '',
+        trainer: '',
+        duration: 0, // days
+        startDate: '',
+        amount: 0,
+    },
+    {
+        code: '',
+        name: '',
+        description: '',
+        trainer: '',
+        duration: 0, // days
+        startDate: '',
+        amount: 0,
+    },
+    {
+        code: '',
+        name: '',
+        description: '',
+        trainer: '',
+        duration: 0, // days
+        startDate: '',
+        amount: 0,
+    },
+]
+
+const table = document.getElementById('courselist')
+
+var count = 1;
+courses.forEach((course, index) => {
+    var row = table.insertRow(count++);
+    var code = row.insertCell(0);
+    var name = row.insertCell(1);
+    var trainer = row.insertCell(2);
+    var date = row.insertCell(3);
+
+    row.id = index;
+    code.innerHTML = course.code;
+    name.innerHTML = course.name;
+    trainer.innerHTML = course.trainer;
+    date.innerHTML = course.startDate;
+
+    row.addEventListener('click', viewCourse)
+});
+
+function viewCourse(event) {
+    courseDetail = document.getElementById('course-detail')
+    courseDetail.innerHTML = ''
+
+    var courseDet = document.createElement('div')
+    courseDet.className = 'details'
+    
+    var courseDesc = document.createElement('div')
+    courseDesc.className = 'desc'
+    var dect = document.createElement('h2')
+    dect.innerHTML = 'Description'
+    courseDesc.appendChild(dect)
+    courseDesc.innerHTML += courses[event.target.parentElement.id].description;
+
+    courseDetail.appendChild(courseDet)
+    courseDetail.appendChild(courseDesc)
+    
+    var heading = document.createElement('h1')
+    heading.innerHTML = courses[event.target.parentElement.id].name;
+    courseDet.appendChild(heading)
+
+    var trainer = document.createElement('div')
+    trainer.innerHTML = 'Trainer: ' + courses[event.target.parentElement.id].trainer
+    courseDet.appendChild(trainer)
+
+    var duration = document.createElement('div')
+    duration.innerHTML = 'Duration: ' + courses[event.target.parentElement.id].duration
+    courseDet.appendChild(duration)
+
+    var date = document.createElement('div')
+    date.innerHTML = 'Date: ' + courses[event.target.parentElement.id].startDate
+    courseDet.appendChild(date)
+
+    var amount = document.createElement('div')
+    amount.innerHTML = 'Amount: ' + courses[event.target.parentElement.id].amount
+    courseDet.appendChild(amount)
+
+    courseDetail.style.display = 'block';
+    console.log(event.target.parentElement.id);
+}
