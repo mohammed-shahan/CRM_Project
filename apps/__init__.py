517ea20d12b794053118d1bb1776908925c19892
akshaylals <akshaylal.s@inapp.com>
2022-08-26 10:35:04 +0530

    database

--- apps/__init__.py
+++ apps/__init__.py
@@ -5,7 +5,7 @@ from importlib import import_module
 from .config import Config
 
 # import for creating test items, can be removed
-# from .models import Categories, Trainers, Courses
+from .models import Categories, Trainers, Courses
 
 
 appsList = (
@@ -28,13 +28,12 @@ def create_app():
 
 
 def setup_database(flask_app):
-    pass
-#     with flask_app.app_context():
-#         db.create_all()
+    with flask_app.app_context():
+        db.create_all()
 
-#         db.session.add(Categories('Web'))
-#         db.session.add(Categories('Mobile'))
-#         db.session.add(Trainers('Abhilash'))
-#         db.session.add(Courses('flask', 'python flask', 4, 1, 1))
-#         db.session.commit()
+        # db.session.add(Categories('Web'))
+        # db.session.add(Categories('Mobile'))
+        # db.session.add(Trainers('Abhilash'))
+        # db.session.add(Courses('flask', 'python flask', 4, 1, 1))
+        db.session.commit()
 
--- apps/models.py
+++ apps/models.py
@@ -1,17 +1,88 @@
 from .database import db
 
 
-class Users(db.Model):    # name of table, flask will translate the Name to lowercase "users"
-    # class attributes, not instance attributes
-    # if using multiple threads to create user, I guess attributes will be easily overwritten and make a mess here
+class Categories(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String, unique=True, nullable=False)
-    email = db.Column(db.String, unique=True, nullable=False)
-    profile = db.Column(db.String, unique=False, nullable=False)
+    category = db.Column(db.String(20), unique=True)
+    courses = db.relationship('Courses')
+
+    def __init__(self, category) -> None:
+        self.category = category
 
 
-class InternalUse(db.Model):     # name of table, flask will translate CamelCase to snake_case "internal_use"
+class Trainers(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    browser_open = db.Column(db.Integer, unique=False, nullable=True)
-    statistics = db.Column(db.String, unique=False, nullable=True)
-    commercials = db.Column(db.String, unique=False, nullable=True)
+    trainer = db.Column(db.String(20), unique=True)
+    courses = db.relationship('Courses')
+
+    def __init__(self, trainer) -> None:
+        self.trainer = trainer
+
+
+class Courses(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50))
+    durationWeeks = db.Column(db.Integer)
+    description = db.Column(db.String(100))
+    
+    category = db.Column(db.Integer, db.ForeignKey('categories.id'))
+    trainer = db.Column(db.Integer, db.ForeignKey('trainers.id'))
+
+    def __init__(self, name, description, durationWeeks, category, trainer) -> None:
+        self.name = name
+        self.description = description
+        self.durationWeeks = durationWeeks
+        self.category = category
+        self.trainer = trainer
+
+
+class Roles(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    role = db.Column(db.String(20), unique=True)
+
+    users = db.relationship('Users')
+
+    def __init__(self, role) -> None:
+        self.role = role
+
+
+
+class Qualifications(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    qualification = db.Column(db.String(20), unique=True)
+
+    users = db.relationship('UserQualifications')
+
+    def __init__(self, qualification) -> None:
+        self.qualification = qualification
+
+class UserQualifications(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    user = db.Column(db.Integer, db.ForeignKey('users.id'))
+    qualification = db.Column(db.Integer, db.ForeignKey('qualifications.id'))
+
+
+
+        
+
+
+        
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    firstName = db.Column(db.String(20))
+    lastName = db.Column(db.String(20))
+    username = db.Column(db.String(50))
+    password = db.Column(db.String(50))
+    email = db.Column(db.String(50))
+
+    role = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+    qualifications = db.relationship('UserQualifications')
+
+    def __init__(self, firstName, lastName, username, password, email, role) -> None:
+        self.firstName = firstName
+        self.lastName = lastName
+        self.username = username
+        self.password = password
+        self.email = email
+        self.role = role
