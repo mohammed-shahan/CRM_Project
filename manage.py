b7745bf2b7d936cf26d4a8106b414cd594137fbc
akshaylals <akshaylal.s@inapp.com>
2022-08-30 12:19:49 +0530

    dummy data

--- manage.py
+++ manage.py
@@ -15,7 +15,18 @@ def deploy():
     migrate()
     upgrade()
 
+def dummy():
+    from apps import create_app
+    from apps.database import db
+    from apps.models import Categories
 
+    app = create_app()
+    app.app_context().push()
+
+    for category in set('Lorem ipsum dolor sit amet consectetur adipiscing elit Donec vel sem nulla Curabitur feugiat hendrerit quam eleifend venenatis Morbi nulla dolor dictum ut rutrum at fermentum a felis Fusce tincidunt aliquam elementum Sed sollicitudin leo lorem id luctus quam rutrum et Maecenas vitae vehicula nisi Proin sit amet lectus eu sapien dictum dictum Aenean vulputate risus'.split()):
+        db.session.add(Categories(category))
+    db.session.commit()
+    
 
 if __name__ == '__main__':
     deploy()
