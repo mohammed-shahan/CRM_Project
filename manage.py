dba223d9b0f39fcdba1979711fd5ff87b6685cc5
akshaylals <akshaylal.s@inapp.com>
2022-09-02 18:34:50 +0530

    dummy course data with rating

--- manage.py
+++ manage.py
@@ -90,6 +90,15 @@ def dummy():
                 Qualifications.query.filter_by(qualification=q).first().id,
                 'Enabled'==status
             ))
+        db.session.add(Courses('Test 1', 'test test', 3, 1, 3, 'example.com', 'comment', 1, True))
+        db.session.add(Courses('Test 2', 'test test', 3, 1, 3, 'example.com', 'comment', 1, True))
+        db.session.add(Courses('Test 3', 'test test', 3, 1, 3, 'example.com', 'comment', 1, True))
+        db.session.add(Courses('Test 4', 'test test', 3, 1, 3, 'example.com', 'comment', 1, True))
+        db.session.commit()
+        Courses.query.filter_by(name='Test 1').first().rating = 5
+        Courses.query.filter_by(name='Test 2').first().rating = 3.5
+        Courses.query.filter_by(name='Test 3').first().rating = 2.7
+        Courses.query.filter_by(name='Test 4').first().rating = 1.3
         db.session.commit()
 
     
